1b4d24c49c208c4f696af822ea6d307f2e2006da
A-Trickster <72161733+A-Trickster@users.noreply.github.com>
2023-05-09 17:43:05 +0000

    start + added random words

---
+++ hangman.py
@@ -0,0 +1,13 @@
+# filename = "s_sequence_3.txt"
+
+# def openFile(filename):
+#     with open(filename, "r") as file:
+#         text = file.read()
+
+#     return text
+
+# words = openFile(filename)
+words= ["apple","toast","Beat","Beautiful","Bed","Belated","Believe","Below","Best","Better","Billion","Blood","Bloom","Book"]
+
+def game(word):
+
