63069d7a6b1a484fbe4233ad4d361f4ce4b9e5af
A-Trickster <72161733+A-Trickster@users.noreply.github.com>
2023-05-09 17:46:59 +0000

    able to get a random word from list

--- hangman.py
+++ hangman.py
@@ -7,7 +7,13 @@
 #     return text
 
 # words = openFile(filename)
+import random
+
 words= ["apple","toast","Beat","Beautiful","Bed","Belated","Believe","Below","Best","Better","Billion","Blood","Bloom","Book"]
 
-def game(word):
+word= random.choice(words)
+print(word)
+
+# def game(word):
+
 
