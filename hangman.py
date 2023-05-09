df11651007bd9cb527884f2b496fc6ab9b042f08
A-Trickster <72161733+A-Trickster@users.noreply.github.com>
2023-05-09 19:39:20 +0000

    gave input + makes words&guess upper

--- hangman.py
+++ hangman.py
@@ -12,14 +12,17 @@ import random
 words= ["apple","toast","Beat","Beautiful","Bed","Belated","Believe","Below","Best","Better","Billion","Blood","Bloom","Book"]
 
 def choseWord(words):
-    return random.choice(words)
+    return random.choice(words).upper()
 
 def game(word):
     # number of tries given to the player
     turns= len(word)+3
     print(turns)
     print(word)
-    return 0
+
+    guess = input("Guess a letter: ").upper()
+    print(guess)
+    
 
 # test whole game
 game(choseWord(words))
